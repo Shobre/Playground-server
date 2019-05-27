51e59bf651150b0e5be23fd36b49b2223bea10e7
Charles Diep <charlesdiep91@gmail.com>
2019-05-27 02:48:01 +0200

    new test

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,8 @@
-const functions = require('firebase-functions');
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// require both the firebase function package to define function   // behavior and your local server config function
+const functions = require("firebase-functions");
+const configureServer = require("./server");
+//initialize the server
+const server = configureServer();
+// create and export the api
+const api = functions.https.onRequest(server);
+module.exports = { api: api };
